e6e1be3875eb9798e293f1cc036c8d1bb5b3b70b
pxai <pello.altadill@gluckgames.com>
2020-10-07 10:47:45 +0200

    Adding views

--- polls/urls.py
+++ polls/urls.py
@@ -4,4 +4,11 @@ from django.urls import path
 urlpatterns = [
     path('about', views.about, name='about'),
     path('', views.index, name='index'),
+    path('index_long', views.index_long, name='index_long'),
+    # ex: /polls/5/
+    path('<int:question_id>/', views.detail, name='detail'),
+    # ex: /polls/5/results/
+    path('<int:question_id>/results/', views.results, name='results'),
+    # ex: /polls/5/vote/
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
