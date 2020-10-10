7d516b23c1716e7d95cf3ca57b9a9aaffacc7853
pxai <pello.altadill@gluckgames.com>
2020-10-10 20:28:10 +0200

    Added simple test

--- polls/test.question.py
+++ polls/test.question.py
@@ -1,13 +1,12 @@
 from django.test import TestCase
-from .models import Question
-from django.utils import timezone
+from ..models import Question
 
 class QuestionTestCase(TestCase):
     def setUp(self):
-        Question.objects.create(question_text="Sample", pub_date=timezone.now())
+        Question.objects.create(question_text="Sample")
 
     def test_question_has_text(self):
         """Animals that can speak are correctly identified"""
         question = Question.objects.get(question_text="Sample")
 
-        self.assertEqual(question.question_text, "Sample")
+        self.assertEqual(question.question_text, "Sample"))
