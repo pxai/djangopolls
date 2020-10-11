f31752f0d89060709ba71785eb82060604cd3cdf
pxai <pello.altadill@gluckgames.com>
2020-10-11 17:19:34 +0200

    Changed test

--- polls/tests.py
+++ polls/tests.py
@@ -1,13 +1,1 @@
 from django.test import TestCase
-from .models import Question
-from django.utils import timezone
-
-class QuestionTestCase(TestCase):
-    def setUp(self):
-        Question.objects.create(question_text="Sample", pub_date=timezone.now())
-
-    def test_question_has_text(self):
-        """Animals that can speak are correctly identified"""
-        question = Question.objects.get(question_text="Sample")
-
-        self.assertEqual(question.question_text, "Sample")
--- polls/tests/tests.question.py
+++ polls/tests/tests.question.py
@@ -1,12 +1,13 @@
 from django.test import TestCase
-from ..models import Question
+from .models import Question
+from django.utils import timezone
 
 class QuestionTestCase(TestCase):
     def setUp(self):
-        Question.objects.create(question_text="Sample")
+        Question.objects.create(question_text="Sample", pub_date=timezone.now())
 
     def test_question_has_text(self):
         """Animals that can speak are correctly identified"""
         question = Question.objects.get(question_text="Sample")
 
-        self.assertEqual(question.question_text, "Sample"))
+        self.assertEqual(question.question_text, "Sample")
