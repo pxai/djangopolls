e1dbac4f964ebcb00af60eee993979bc412025ab
pxai <pello.altadill@gluckgames.com>
2020-10-14 11:09:16 +0200

    Added generic views

--- polls/urls.py
+++ polls/urls.py
@@ -4,12 +4,16 @@ from django.urls import path
 app_name = 'polls'  # now you have to add `polls:` to links
 urlpatterns = [
     path('about', views.about, name='about'),
-    path('', views.index, name='index'),
+    # manual # path('', views.index, name='index'),
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
     path('index_long', views.index_long, name='index_long'),
     # ex: /polls/5/
-    path('<int:question_id>/', views.detail, name='detail'),
+    # manual # path('<int:question_id>/', views.detail, name='detail'),
     # ex: /polls/5/results/
-    path('<int:question_id>/results/', views.results, name='results'),
+    # manual # path('<int:question_id>/results/', views.results, name='results'),
     # ex: /polls/5/vote/
-    path('<int:question_id>/vote/', views.vote, name='vote'),
+    # manual # path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
