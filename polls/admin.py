958c535ca1c43199e1c1542a25a083b18ddf6378
pxai <pello.altadill@gluckgames.com>
2020-10-24 13:05:34 +0200

    Add admin more filter options

--- polls/admin.py
+++ polls/admin.py
@@ -12,7 +12,9 @@ class QuestionAdmin(admin.ModelAdmin):
         ('Date information', {'fields': ['pub_date']}),
     ]
     inlines = [ChoiceInline]
-
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text', 'pub_date']
 
 admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
