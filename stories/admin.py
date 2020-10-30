1ac759b8376c06f853a6198608b3fd515b772f58
pxai <pello.altadill@gluckgames.com>
2020-10-30 22:25:42 +0100

    corrected stuff

--- stories/admin.py
+++ stories/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Story
 
-# Register your models here.
+@admin.register(Story)
+class StoryAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'author', 'publish', 'status')
+    list_filter = ('status', 'created', 'publish', 'author')
+    search_fields = ('title', 'body')
+    prepopulated_fields = {'slug': ('title',)}
+    raw_id_fields = ('author',)
+    date_hierarchy = 'publish'
+    ordering = ('status', 'publish')
